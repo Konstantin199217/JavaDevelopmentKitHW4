da4a214f04f8c9cabe2ed4239d17fae115a4bd8b
Konstantin <kot199217@mail.ru>
2023-11-25 17:39:53 +0900

    HW1.1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,71 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/**
+ * Создать справочник сотрудников
+ * Необходимо:
+ * Создать класс справочник сотрудников, который содержит внутри
+ * коллекцию сотрудников - каждый сотрудник должен иметь следующие атрибуты:
+ * - Табельный номер
+ * - Номер телефона
+ * - Имя
+ * - Стаж
+ * Добавить метод, который ищет сотрудника по стажу (может быть список)
+ * Добавить метод, который возвращает номер телефона сотрудника по имени (может быть список)
+ * Добавить метод, который ищет сотрудника по табельному номеру
+ * Добавить метод добавление нового сотрудника в справочник
+ */
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        WorkerCatalog catalog = new WorkerCatalog();
+        catalog.addWorker(
+                new Worker(
+                        01,
+                        "89123123123",
+                        "Иванов Иван Иванович",
+                        2));
+        catalog.addWorker(
+                new Worker(
+                        02,
+                        "892222222222",
+                        "Петров Петр Петрович",
+                        2));
+        catalog.addWorker(
+                new Worker(
+                        03,
+                        "89333333333",
+                        "Сидоров Олег Георгевич",
+                        3));
+        catalog.addWorker(
+                new Worker(
+                        04,
+                        "89444444444",
+                        "Берсерков Гатс Александрович",
+                        3));
+        catalog.addWorker(
+                new Worker(
+                        05,
+                        "8955555555",
+                        "Пушкин Александр Сергеевич",
+                        4));
+        catalog.addWorker(
+                new Worker(
+                        06,
+                        "8966666666",
+                        "Бонд Джеймс Бондович",
+                        19));
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Сотрудники добавленные с каталог");
+        System.out.println(catalog.printCatalog());
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("\nПолучение сотрудника по стажу");
+        System.out.println("Сотрудники со стажем менее 5 лет: " +
+                catalog.findByFromMinExperience(5).toString() + "\n");
+        System.out.println("Сотрудники со стажем более 5 лет: " +
+                catalog.findByFromMaxExperience(5).toString() + "\n");
+
+        System.out.println("\nПолучение сотрудника по номеру телефона");
+        System.out.println("Получение сотрудника с номером 89444444444: " + catalog.findByPhoneNumber("89444444444"));
+
+        System.out.println("\nПолучение сотрудника по табельному номеру ");
+        System.out.println("Получение сотрудника с номером 03: " + catalog.findByPersonnelNumber(03));
     }
 }
--- src/Worker.java
+++ src/Worker.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Worker {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Класс сотрудника.
+ */
+@Getter // Аннотация lombok - добавляет геттеры ко всем полям
+@AllArgsConstructor // Аннотация lombok - добавление конструктора для всех полей
+public class Worker {
+    private long personnelNumber;
+    private String phoneNumber;
+    private String fullName;
+    private Integer experience;
+
+    @Override
+    public String toString() {
+        return String.format("Работник: %s табельный номер: %d, телефон: %s, рабочий стаж: %d лет",
+                this.fullName, this.personnelNumber, this.phoneNumber, this.experience);
+    }
 }
